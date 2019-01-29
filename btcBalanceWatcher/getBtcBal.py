327f74297d8af3b984c92ca41ce5a960cf7a1754
Harsh Singh <harsh.crypto@gmail.com>
2019-01-29 16:56:53 -0500

    revision

--- btcBalanceWatcher/getBtcBal.py
+++ btcBalanceWatcher/getBtcBal.py
@@ -1,10 +1,23 @@
 import requests
 import csv
+import os
+
+#function to notify
+def notify(title, text):
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+def writeResult(result):
+    file = open('result.txt','w')
+    file.write(result)
+    file.close()
 
 with open('file-list.txt') as csv_file:
     file_list_reader = csv.reader(csv_file, delimiter=',')
     file_count = 0
     totalBalance=0
+    notification=""
+    hackedAddress=""
     for row in file_list_reader:
         fileName=row[0]
         print('Processing File:' ,fileName)
@@ -22,8 +35,10 @@ with open('file-list.txt') as csv_file:
                     resp = requests.get(url)
                     currentBalance = float(resp.text)
                     if balance != currentBalance:
-                        print('\t**************************Hacked address:', address,' original balance', balance ,'currentBalance', currentBalance)
-                    print('\tSafe address:', address,' original balance', balance ,'currentBalance', currentBalance)
+                        hackedAddress+=fileName+':' + str(address) + '\n'
+                        hacked='\tATTN: HACKED address:'+ str(address)+' original balance'+ str(balance)+' currentBalance'+str(currentBalance)
+                        print(hacked)
+                    print('\tSafe address:', address,' original balance', balance ,' currentBalance', currentBalance)
                     totalWalletBalance+=float(balance)
                     line_count += 1
             print("Total Balance:",totalWalletBalance/100000000)
@@ -33,14 +48,24 @@ with open('file-list.txt') as csv_file:
 
 print('Processed', file_count,' files.')
 print("Total Balance:",totalBalance/100000000)
-
+if hackedAddress=="":
+    notification += "SAFE: "
+    notification += str(totalBalance/100000000)
+else:
+    notification += "HACKED: "
+    notification += hackedAddress
 spoofAddress = open('spoofAddress.txt')
 
 spoofLine = spoofAddress.readline()
 while spoofLine:
-    #print("Spoof Address:"+ spoofLine)
     r = requests.get("https://blockexplorer.com/api/addr/"+spoofLine+"/balance")
     spoofLine = spoofAddress.readline()
 spoofAddress.close()
+notify("Wallet", notification)
+writeResult(notification)
+
+
+
+
 
 
